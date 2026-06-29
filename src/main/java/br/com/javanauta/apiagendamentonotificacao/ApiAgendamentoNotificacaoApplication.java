c4b6a476965ce74666f46734e8109e5906fe0674
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-29 17:41:43 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/javanauta/apiagendamentonotificacao/ApiAgendamentoNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.javanauta.apiagendamentonotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendamentoNotificacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAgendamentoNotificacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/javanauta/apiagendamentonotificacao/ApiAgendamentoNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javanauta.apiagendamentonotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendamentoNotificacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
