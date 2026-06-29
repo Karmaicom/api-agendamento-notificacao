983df0b2a5a9c05a4721194198a54fae9258c61a
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-29 18:57:37 -0300

    Incluindo dependencias do mapstruct. Inclusao de campos no record de response.

--- src/main/java/br/com/javanauta/apiagendamentonotificacao/dtos/AgendamentoResponseDTO.java
+++ src/main/java/br/com/javanauta/apiagendamentonotificacao/dtos/AgendamentoResponseDTO.java
@@ -1,4 +1,17 @@
 package br.com.javanauta.apiagendamentonotificacao.dtos;
 
-public record AgendamentoResponseDTO() {
+import br.com.javanauta.apiagendamentonotificacao.entities.enums.StatusNoficacaoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoResponseDTO(
+        Long id,
+        String emailDestinatorio,
+        String telefoneDestinatario,
+        String mensagem,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+        LocalDateTime dataHoraEnvio,
+        StatusNoficacaoEnum statusNoficacao
+) {
 }
